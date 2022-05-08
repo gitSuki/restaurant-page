c462a0b65f246395e96ac95ff9f290611a1446d3
gitSuki <collinbrown226@gmail.com>
2022-05-07 22:22:37 -0500

    Fix Webpack bug

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Webpack test")
+import printMe from './print.js';
+
+function component() {
+    const element = document.createElement('div')
+    element.textContent = "hi"
+
+    return element
+}
+
+document.body.appendChild(component())
+printMe()
+console.log("Webpack test 2")
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
+    index: './src/print.js',
     index: './src/index.js',
   },
   devtool: 'inline-source-map',
