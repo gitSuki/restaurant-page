d84aadb302f9fcf63783ea848b50b13fce74aa4a
gitSuki <collinbrown226@gmail.com>
2022-05-13 20:15:13 -0500

    Refactor contact DOM code

--- src/contact.js
+++ src/contact.js
@@ -6,10 +6,41 @@ export default function buildContactTab(){
 
     const newContent = document.createElement('div')
     newContent.id = "content"
-    
-    const contactTitle = document.createElement('span')
-    contactTitle.textContent = "Contact Us!"
-    newContent.appendChild(contactTitle)
+    newContent.appendChild(contactTitle())
+    newContent.appendChild(contactContent())
+    newContent.classList.add("contact-content")
 
     page.replaceChild(newContent, oldContent)
+}
+
+function contactTitle(){
+    const contactTitle = document.createElement('span')
+    contactTitle.textContent = "Contact Us!"
+    contactTitle.classList.add("contact-title")
+
+    return contactTitle
+}
+
+function contactContent(){
+    const contactContainer = document.createElement('form')
+    contactContainer.setAttribute("method", "post")
+
+
+    contactContainer.appendChild(name())
+
+    contactContainer.classList.add("contact-container")
+
+    return contactContainer
+}
+
+function name(){
+    const nameContainer = document.createElement('div')
+
+    const nameInput = document.createElement('input')
+    nameInput.setAttribute("type", "text")
+    nameInput.setAttribute("name", "name")
+    nameInput.setAttribute("placeholder", "name")
+    nameContainer.appendChild(nameInput)
+
+    return nameContainer
 }
